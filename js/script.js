97a2fff961a8f39115d98650fdb38fa03016621c
Joanna Panimasz <pani.joanna93@gmail.com>
2022-05-17 15:40:34 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let plnRate = document.querySelector(".js-pln");
-let currencyRate = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form")
+{
+    const welcome = () => {
+        console.log("Hello, developers!");
+    }
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let pln = plnRate.value;
-
-    let convert = pln * 4.71;
-    currencyRate.value = convert.toFixed(2);
-});
+    welcome();
+    
+    const onChangeCurrencyClick = (event) => {
+        event.preventDefault();
+        let pln = plnRate.value;
+    
+        let convert = pln * 4.71;
+        currencyRate.value = convert.toFixed(2);
+    };
+    
+    let plnRate = document.querySelector(".js-pln");
+    let currencyRate = document.querySelector(".js-currency");
+    let formElement = document.querySelector(".js-form")
+    
+    formElement.addEventListener("submit", onChangeCurrencyClick);
+    welcome();
+}
