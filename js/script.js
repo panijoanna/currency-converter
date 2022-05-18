1a84dc58c8e8334914e3f5626cf76fcaeff19456
Joanna Panimasz <pani.joanna93@gmail.com>
2022-05-18 11:31:29 +0200

    new functions added

--- js/script.js
+++ js/script.js
@@ -5,16 +5,19 @@
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+        const plnRate = document.querySelector(".js-pln");
+        const currencyRate = document.querySelector(".js-currency");
         const pln = plnRate.value;
-    
+
         const convert = pln * 4.71;
         currencyRate.value = convert.toFixed(2);
     };
-    
-    const plnRate = document.querySelector(".js-pln");
-    const currencyRate = document.querySelector(".js-currency");
-    const formElement = document.querySelector(".js-form");
-    
-    formElement.addEventListener("submit", onFormSubmit);
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
     welcome();
+    init();
 }
